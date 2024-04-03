7bda83f0eae9fadf7516ce2434b18df57562169d
Brian Morales <joseph.fickas@gmail.com>
2024-04-03 12:06:21 -0600

    Trabajo de investigacion

---
+++ CRUD_FRAMEWORK_YOUTUBE/Controllers/BookController.cs
@@ -0,0 +1,118 @@
+﻿using CRUD_FRAMEWORK_YOUTUBE.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_FRAMEWORK_YOUTUBE.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Books.ToList());
+            }
+
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Books.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(Book books)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    context.Books.Add(books);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Books.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Book books)
+        {
+            try
+            {
+                using (DbModels context = new DbModels())
+                {
+                    context.Entry(books).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels context = new DbModels())
+            {
+                return View(context.Books.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DbModels context = new DbModels())
+                {
+                    Book book = context.Books.Where(x=>x.id == id).FirstOrDefault();
+                    context.Books.Remove(book);
+                    context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUD_FRAMEWORK_YOUTUBE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUD_FRAMEWORK_YOUTUBE.MvcApplication" Language="C#" %>
